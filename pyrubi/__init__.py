3249a15d9e3d0da9274da133f002fcd74390ef4d
Ali Ganji zadeh <120641959+AliGanji1@users.noreply.github.com>
2023-12-14 15:33:58 +0330

    Delete pyrubi directory

--- pyrubi/__init__.py
+++
@@ -1,8 +0,0 @@
-from .methods import methods as Bot
-from .message import message as Message
-from .tools import tools as Tools
-
-__version__ = '2.1.1'
-__author__ = 'Ali Ganji zadeh'
-__github__ = 'https://github.com/AliGanji1/pyrubi'
-__rubika__ = '@pyrubika'
--- pyrubi/servers/__init__.py
+++
@@ -1,9 +0,0 @@
-from random import choice
-from urllib3 import PoolManager
-from json import loads
-
-def get_server(type):
-    if type == 'api':
-        return f'https://messengerg2c2.iranlms.ir/'
-    else:
-        return choice(list(loads(PoolManager().request('GET', 'https://getdcmess.iranlms.ir/').data.decode())['data']['socket'].values()))
